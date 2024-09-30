ee76a6fd4d29d1f4878a0d933131f59204fd51fc
CherryLiu <cherryliuliuchen@gmail.com>
2024-09-30 09:52:25 +0200

    admin commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,60 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import MyNavbar from './components/Navbar';
+// import Footer from './components/Footer';
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import UserManagementPage from './pages/UserManagementPage';
+import UserCreatePage from './pages/UserCreatePage';
+import MyInformationPage from './pages/MyInformationPage';
+import PrivateRoute from './components/PrivateRoute';
+import AccessChangePage from './pages/AccessChangePage';
+import './styles/global.css';
 
-function App() {
+
+const App: React.FC = () => { //It is a constant.
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="d-flex flex-column min-vh-100">
+      <Router>
+        <MyNavbar />
+        <div className="container flex-grow-1">
+          <Routes>
+            {/* Public page */}
+            <Route path="/" element={<HomePage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/access-change" element={<AccessChangePage />} />
+
+            {/* Protected page */}
+            <Route
+              path="/user-management"
+              element={
+                <PrivateRoute>
+                  <UserManagementPage />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/create-user"
+              element={
+                <PrivateRoute>
+                  <UserCreatePage />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/my-information"
+              element={
+                <PrivateRoute>
+                  <MyInformationPage />
+                </PrivateRoute>
+              }
+            />
+          </Routes>
+        </div>
+        {/* <Footer /> */}
+      </Router>
     </div>
   );
-}
+};
 
 export default App;
