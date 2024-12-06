bb388f0c287f0c07424e9aacaeba52133c379d31
Cherry Chen Liu <105421663+cherryliuliuchen@users.noreply.github.com>
2024-12-06 20:06:24 +0100

    Remove frontend-user directory

--- frontend-user/src/api/apiConfig.jsx
+++
@@ -1,3 +0,0 @@
-// src/api/apiConfig.js
-// export const API_BASE_URL = 'http://localhost:5000/api';
-export const API_BASE_URL = 'https://foodapp-cqdcbbc6d8gqambb.germanywestcentral-01.azurewebsites.net/api';
