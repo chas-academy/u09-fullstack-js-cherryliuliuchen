55e42e38f13ed12ad6f83791b4a8eb83e1d270f7
Cherry Chen Liu <105421663+cherryliuliuchen@users.noreply.github.com>
2024-12-06 19:40:09 +0100

    last version

--- vite-project/src/components/Navbar.jsx
+++ vite-project/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
-import { Link } from 'react-router-dom'; // 使用react-router-dom中的Link组件
+import { Link } from 'react-router-dom';
 
 const MyNavbar = ({ loggedIn }) => {
   return (
--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,11 +1,10 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
-import ReactDOM from 'react-dom/client'; // React 18 使用 react-dom/client
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { registerSW } from 'virtual:pwa-register';
 
-// 注册 PWA 更新
 const updateSW = registerSW({
   onNeedRefresh() {
     console.log('New content is available. Refresh the page to update.');
@@ -15,11 +14,11 @@ const updateSW = registerSW({
   },
 });
 
-// 使用 createRoot 启动应用（React 18）
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter> {/* 保留 BrowserRouter */}
+    <BrowserRouter> 
       <App />
     </BrowserRouter>
   </React.StrictMode>
